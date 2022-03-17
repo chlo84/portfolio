ff95172bb5d2b47dbed9bf14ad8a80dc2d7967bb
Chloe <chloe84@msn.com>
2022-03-17 18:31:54 -0400

    added buttons and tags to the carousel added background color

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
+import {Button, Card} from 'react-bootstrap';
+
 function Footer () {
 return (
     <div className='page footerPage' id='contact'>
         <h1>Footer</h1>
+        <Button>Click here for more Information</Button>
+        <Card>
+            <Card.Header>Quote</Card.Header>
+            <Card.Body>
+                <blockquote className="blockquote mb-0">
+                    <p>
+                        {' '}
+                        Test a Quote
+                        {' '}
+                    </p>
+                    <footer className="blockquote-footer">
+                        Someone famous in <cite title="Source Title">Source Title</cite>
+                    </footer>
+                </blockquote>
+            </Card.Body>
+        </Card>
     </div>
 )
 }
--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -5,40 +5,20 @@ return (
     <div className='page projectsPage' id='projects'>
         <h1>Projects</h1>
         <Carousel>
-  <Carousel.Item interval={1000}>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=First slide&bg=373940"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item interval={500}>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Second slide&bg=282c34"
-      alt="Second slide"
-    />
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Third slide&bg=20232a"
-      alt="Third slide"
-    />
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
+          <Carousel.Item interval={1000}>
+              <h1>Calculator Project</h1>
+              <p>This is my first project as a learning Developer, she is my first Developer baby and this project shows just how far I have come!</p>
+              <button>View my Handheld Calculator Project</button>
+          </Carousel.Item>
+          <Carousel.Item interval={500}>
+              <h1>Game Project</h1>
+              <p>The game project was my second child as a learning Developer, maybe even you can see how far I have come</p>
+              <button>Let's See!</button>
+          </Carousel.Item>
+          <Carousel.Item>
+              <p>This is my third project</p>
+          </Carousel.Item>
+        </Carousel>
     </div>
 )
 }
