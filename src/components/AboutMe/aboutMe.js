3e2ca9c0c59a959a9529350537ea2d49498cae19
Chloe <chloe84@msn.com>
2022-05-23 22:27:16 -0400

    moved image to landing page css added an about me description

--- src/components/AboutMe/aboutMe.js
+++ src/components/AboutMe/aboutMe.js
@@ -4,7 +4,7 @@ import Button from 'react-bootstrap/Button';
 function AboutMe () {
 return (
     <div className='page AboutMe' id='about'>
-        <h1>AboutMe</h1>
+        <h1>A Junior Developer with Advocacy experience with a mission to help victims. After spending years as an Advocate I decided to go on another mission as a Developer to explore the IT world!</h1>
     </div>
 )
 
